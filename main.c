9ab2540ed5b130520f066c85a8ca54104240e83c
Jose Fernando Nevarez Vasquez <jose060304abc@gmail.com>
2026-08-12 16:10:18 -0500

    Implemented random doors (not yet interactable)

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <ncurses.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct {
     int x;
@@ -21,6 +22,8 @@ typedef struct{
     Position position;
     int height;
     int width;
+
+    Position ** doors;
     //Monster ** monsters;
     //Item ** items;
 } Room;
@@ -69,6 +72,8 @@ int screenSetup(){
     noecho();
     refresh();
 
+    srand(time(NULL));
+
     return 1;
 }
 
@@ -112,6 +117,28 @@ Room * createRoom(int x, int y, int height, int width){
     newRoom -> height = height;
     newRoom -> width = width;
 
+    newRoom -> doors = malloc(sizeof(Position) * 4);
+
+    /* top door */
+    newRoom -> doors[0] = malloc(sizeof(Position));
+    newRoom -> doors[0] -> x = rand() % (width - 2) + newRoom -> position.x + 1;
+    newRoom -> doors[0] -> y = newRoom -> position.y;
+
+    /* bottom door */
+    newRoom -> doors[1] = malloc(sizeof(Position));
+    newRoom -> doors[1] -> x = rand() % (width - 2) + newRoom -> position.x + 1;
+    newRoom -> doors[1] -> y = newRoom -> position.y + newRoom -> height - 1;
+
+    /* left door */
+    newRoom -> doors[2] = malloc(sizeof(Position));
+    newRoom -> doors[2] -> x = newRoom -> position.x;
+    newRoom -> doors[2] -> y = rand() % (height - 2) + newRoom -> position.y + 1;
+
+    /* right door */
+    newRoom -> doors[3] = malloc(sizeof(Position));
+    newRoom -> doors[3] -> x = newRoom -> position.x + width - 1;
+    newRoom -> doors[3] -> y = rand() % (height - 2) + newRoom -> position.y + 1;
+
     return newRoom;
 }
 
@@ -137,6 +164,12 @@ int drawRoom(Room * room){
             mvprintw(y,x,".");
         }
     }
+
+    /* draw doors */
+    mvprintw(room -> doors[0]->y, room -> doors[0]->x, "+");
+    mvprintw(room -> doors[1]->y, room -> doors[1]->x, "+");
+    mvprintw(room -> doors[2]->y, room -> doors[2]->x, "+");
+    mvprintw(room -> doors[3]->y, room -> doors[3]->x, "+");
 
     return 1;
 }
